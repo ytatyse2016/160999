7ede12734327b3ac8aa30b98419c58cb47ff0dae
Yoshino Taichi <yoshinotaichi@gmail.com>
2016-11-24 11:55:06 +0900

    4回に1回、"ALOHA!"

--- 20161124/Aloha.java
+++ 20161124/Aloha.java
@@ -4,7 +4,14 @@ public class Aloha {
 	public static void main(String[] args) {
 		// 20回表示する
 		for( int i = 0;  i < 20;  i++ ) {
-			System.out.println("アロハ！");
+
+			// 4回に1回、"ALOHA!"
+			if( i%4 == 0 ) {
+				System.out.println("ALOHA!");
+			} else {
+				System.out.println("アロハ！");
+			}
+
 		}
 	}
 }
