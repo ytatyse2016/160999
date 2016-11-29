71d558687221bdb52b8a563bad30a8a5b525503f
Yoshino Taichi <yoshinotaichi@gmail.com>
2016-11-29 09:47:26 +0900

    Aloha.javaをつくりました

---
+++ 20161129/Aloha.java
@@ -0,0 +1,11 @@
+// Aloha.java
+// 2. "ALOHA!"と20回出力する
+// 3. ↑4回に1回、"アロハ！"と出力する
+// 4. args[0]をつかって、「～さん、アロハ！」と出力する
+// 実行: java Aloha  Aoki
+
+public class Aloha {
+	public static void main(String[] args) {
+		System.out.println(args[0] + "さん、アロハ！");
+	}
+}
