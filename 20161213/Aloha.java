caf99d5b755a3b487838f188ca7f4cb540cfbc0e
Yoshino Taichi <yoshinotaichi@gmail.com>
2016-12-13 15:13:15 +0900

    Aloha.java updated.

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -2,6 +2,18 @@
 
 public class Aloha {
   public static void main(String[] args) {
-    System.out.println("ALOHA!");
+    // くり返し制御のフラグ
+    boolean flag = true;
+
+    while(flag) {
+      System.out.print("Input message >");
+      String msg = new java.util.Scanner(System.in).nextLine();
+      System.out.println(msg);
+
+      // キーワードで終了.
+      if(msg.equals("exit")) {
+        flag = false;
+      }
+    }
   }
 }
