fc941bdadbf3de96b281497f96ee902caf587309
Yoshino Taichi <yoshinotaichi@gmail.com>
2016-12-20 09:53:43 +0900

    Aloha.java added

---
+++ 20161220/Aloha.java
@@ -0,0 +1,11 @@
+// Aloha.java
+// "ALOHA!"‚ð20‰ñ•\Ž¦‚·‚é.
+
+public class Aloha {
+  public static void main(String[] args) {
+    // 20‰ñ‚­‚è•Ô‚·
+    for(int i = 0;  i < 20;  i++) {
+      System.out.println("ALOHA!");
+    }
+  }
+}
