ee6c081bb5402fd2e538a45b7c437fef164e1f87
Yoshino Taichi <yoshinotaichi@gmail.com>
2016-12-22 09:53:47 +0900

    FrameMan was born.

---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,19 @@
+	import java.awt.*;
+	import javax.swing.*;
+
+	public class MainFrameMan {
+		public static void main(String[] args) {
+			FrameMan fm = new FrameMan();
+		}
+	}
+
+
+	class FrameMan {
+		// データ
+		JFrame frame;
+
+		// メソッド( 処理 )
+		public FrameMan(){
+			System.out.println("ALOHA2!");
+		}
+	}
